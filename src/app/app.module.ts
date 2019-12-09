7a33848ddd8a357c7e1bdfff8f06d72edaeef014
rajesh23-drg <rmanjunath@teamdrg.com>
2019-12-09 14:34:15 +0530

    Added Cart-Details Component, Routing Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router'
+import { HttpClientModule } from '@angular/common/http';
+// Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { ProductItemComponent } from './products/product-list/product-item/product-item.component';
+import { CartDetailsComponent } from './carts/cart-details/cart-details.component';
+// Services
 import { ProductsService } from './products/products.service';
-import { HttpClientModule } from '@angular/common/http';
 import { CartsService } from './carts/carts.service';
+// Routes
+const appRoutes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'cart-details', component: CartDetailsComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ProductListComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    CartDetailsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductsService, CartsService],
   bootstrap: [AppComponent]
